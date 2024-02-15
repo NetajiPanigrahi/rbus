22bbf0b556f6e8d810f67733be05f8738cd6dce3
Netaji Panigrahi <114923459+NetajiPanigrahi@users.noreply.github.com>
2024-02-15 14:14:58 +0530

    Update rbusConsumer.cpp

--- unittests/rbusConsumer.cpp
+++ unittests/rbusConsumer.cpp
@@ -90,12 +90,8 @@ static int exec_rbus_get_test(rbusHandle_t handle, const char *param)
 
   } else if (0 == strcmp(param,"Device.rbuscoreProvider.GetLegBytes")) {
     int len = 0;
-	  printf("%s: %d\n", __FUNCTION__, __LINE__);
-	if (strcmp(rbusValue_GetString(val,NULL), "A") == 0)
-	{
-		printf("%s: %d SUCCESS >>>\n", __FUNCTION__, __LINE__);
-	}
-	  /*
+	  printf("%s: %d %s, \n", __FUNCTION__, __LINE__, rbusValue_GetString(val,NULL));
+	/*
 	    const uint8_t *ptr = rbusValue_GetBytes(val, &len);
 	  printf("ptr data: %d", *(ptr));
    */
