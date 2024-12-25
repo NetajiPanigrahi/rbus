db6a8cdb3efe1da2786f9d853d77fa03bcdd27b9
Netaji Panigrahi <114923459+NetajiPanigrahi@users.noreply.github.com>
2024-12-25 14:53:53 +0530

    Update testlogger.c

--- src/rtmessage/testlogger.c
+++ src/rtmessage/testlogger.c
@@ -1,24 +1,3 @@
-
-/*
-##########################################################################
-# If not stated otherwise in this file or this component's LICENSE
-# file the following copyright and licenses apply:
-#
-# Copyright 2016 RDK Management
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-##########################################################################
-*/
 #include "rtLog.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,14 +16,11 @@
 #define rtLog_Fatal(FORMAT,...) rtLog_FatalPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
 int main(int argc, char * argv[])
 {
-    #ifdef ENABLE_RDKLOGGER
-    rdk_logger_init("/etc/debug.ini");
-#endif
-
+  rdk_logger_init("/etc/debug.ini");
   rtLog_SetOption(RT_USE_RDKLOGGER);
   rtLogSetLogHandler(NULL);
   while(1) {
-      sleep(5);
+      sleep(10);
       rtLog_Error("TEST Message...%ld: %ld", (long)getpid(), (long)getppid());
   }
 }
