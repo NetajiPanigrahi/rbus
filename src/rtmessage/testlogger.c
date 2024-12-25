8fc784c52ad102c1e4617479c932d324970183d3
Netaji Panigrahi <114923459+NetajiPanigrahi@users.noreply.github.com>
2024-12-26 00:29:15 +0530

    Update testlogger.c

--- src/rtmessage/testlogger.c
+++ src/rtmessage/testlogger.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include "rtDebug.h"
 
 #define testLog_WarnPrint(mod,FORMAT...)   rtLogPrintf(RT_LOG_WARN, mod, __FILE__, __LINE__, FORMAT)
 #define testLog_ErrorPrint(mod,FORMAT...)  rtLogPrintf(RT_LOG_ERROR, mod, __FILE__, __LINE__, FORMAT)
@@ -14,7 +15,7 @@
 #define testLog_Warn(FORMAT,...)  testLog_WarnPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
 #define testLog_Error(FORMAT,...) testLog_ErrorPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
 #define testLog_Fatal(FORMAT,...) testLog_FatalPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
-int main(int argc, char * argv[])
+int main()
 {
   rdk_logger_init("/etc/debug.ini");
   rtLog_SetOption(RT_USE_RDKLOGGER);
