afd8a0c9581a0652e3d736c4aa16d99da9a75d8c
Netaji Panigrahi <114923459+NetajiPanigrahi@users.noreply.github.com>
2024-12-26 01:32:56 +0530

    Update testlogger.c

--- src/rtmessage/testlogger.c
+++ src/rtmessage/testlogger.c
@@ -1,4 +1,3 @@
-#include "rtLog.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,22 +5,11 @@
 #include <sys/types.h>
 #include "rdk_debug.h"
 
-#define testLog_WarnPrint(mod,FORMAT...)   rtLogPrintf(RT_LOG_WARN, mod, __FILE__, __LINE__, FORMAT)
-#define testLog_ErrorPrint(mod,FORMAT...)  rtLogPrintf(RT_LOG_ERROR, mod, __FILE__, __LINE__, FORMAT)
-#define testLog_FatalPrint(mod,FORMAT...)  rtLogPrintf(RT_LOG_FATAL, mod, __FILE__, __LINE__, FORMAT)
-  
-#define testLog_Debug(FORMAT,...) testLog_DebugPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
-#define testLog_Info(FORMAT,...)  testLog_InfoPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
-#define testLog_Warn(FORMAT,...)  testLog_WarnPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
-#define testLog_Error(FORMAT,...) testLog_ErrorPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
-#define testLog_Fatal(FORMAT,...) testLog_FatalPrint("TESTLOGGER",FORMAT"\n", ##__VA_ARGS__)
 int main()
 {
   rdk_logger_init("/etc/debug.ini");
-  rtLog_SetOption(RT_USE_RDKLOGGER);
-  rtLogSetLogHandler(NULL);
   while(1) {
-      sleep(10);
-     testLog_Error("TEST Message...%ld: %ld", (long)getpid(), (long)getppid());
+      sleep(5);
+      RDK_LOG(RDK_LOG_ERROR, "LOG.RDK.TESTLOGGER","Message...%ld: %ld\n", (long)getpid(), (long)getppid());
   }
 }
